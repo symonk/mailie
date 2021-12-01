031c141c2f170b31999bcd19547b477d588c6d20
symonk <jackofspaces@gmail.com>
2021-12-01 22:07:55 +0000

    adding --subject and --message

--- mailie/_email.py
+++ mailie/_email.py
@@ -4,16 +4,19 @@ import smtplib
 import typing
 from email.message import EmailMessage
 
+import typer
+
 from ._policy import Policies
 
 
 class Email:
     # version 0.1.0 will encompass a simple plaintext mail; then iterate.
-    def __init__(self, *, frm: str, to: str, policy: str, subject: str):
+    def __init__(self, *, frm: str, to: str, policy: str, subject: str, message: str):
         self.delegate = EmailMessage(Policies.get(policy))
         self.add_header("From", frm)
         self.add_header("To", to)
         self.add_header("Subject", subject)
+        self.set_payload(message)
 
     def __str__(self) -> str:
         return str(self.delegate)
@@ -48,7 +51,8 @@ class Dispatcher:
 
     def send(self) -> Email:
         with smtplib.SMTP(self.host, port=self.port) as smtp:
-            smtp.sendmail(self.message.delegate["From"], [self.message.delegate["To"]], str(self.message))
+            result = smtp.sendmail(self.message.delegate["From"], [self.message.delegate["To"]], str(self.message))
+            typer.secho(result, fg=typer.colors.GREEN, bold=True)
         return self.message
 
 
@@ -56,5 +60,5 @@ class EmailFactory:
     # Todo: Apply the pattern fully.
 
     @classmethod
-    def create(cls, *, frm: str, to: str, subject: str = "", policy: str = "default") -> Email:
-        return Email(frm=frm, to=to, policy=policy, subject=subject)
+    def create(cls, *, frm: str, to: str, subject: str = "", message: str = "", policy: str = "default") -> Email:
+        return Email(frm=frm, to=to, policy=policy, subject=subject, message=message)
--- mailie/mailie.py
+++ mailie/mailie.py
@@ -12,14 +12,14 @@ def mail(
     to: str,
     policy: str = typer.Option("default", case_sensitive=False),
     subject: str = "",
+    message: str = "",
     hostname: str = "",
     port: int = 0,
 ) -> None:
     typer.secho("Mailie is generating a mail.", fg=typer.colors.GREEN, bold=True)
-    with typer.progressbar(range(50)):
-        email = EmailFactory.create(frm=frm, to=to, policy=policy, subject=subject)
-        typer.secho("Sending mail...", fg=typer.colors.GREEN, bold=True)
-        Dispatcher(message=email, host=hostname, port=port).send()
+    email = EmailFactory.create(frm=frm, to=to, policy=policy, message=message, subject=subject)
+    typer.secho("Sending mail...", fg=typer.colors.GREEN, bold=True)
+    Dispatcher(message=email, host=hostname, port=port).send()
 
 
 @app.command()
