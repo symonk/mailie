677c847863fd4211c705e2e01dff24a2aa50a1da
symonk <jackofspaces@gmail.com>
2021-12-04 18:23:46 +0000

    starting to build out a fake smtp sink server

--- tests/dsl/integration/conftest.py
+++ tests/dsl/integration/conftest.py
@@ -1,6 +1,12 @@
+import asyncore
+
 import pytest
+from server import FakeSMTPServer
 
 
 @pytest.fixture()
 def mail_to_disk_server():
-    ...
+    server = FakeSMTPServer(("localhost", 1250), None)
+    asyncore.loop()
+    yield server
+    server.close()
--- tests/dsl/integration/server.py
+++ tests/dsl/integration/server.py
@@ -1,6 +1,12 @@
+from email.parser import Parser
 from smtpd import SMTPServer
 
 
 class FakeSMTPServer(SMTPServer):
+    total = 0
+    files = []
+
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        pass
+        _ = Parser().parsestr(data)
+        # TODO: Finish this off.
+        ...
