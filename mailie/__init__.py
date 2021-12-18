52a151e6ad102ddf5327bd790bbe3a54f4729544
symonk <jackofspaces@gmail.com>
2021-12-18 14:34:51 +0000

    starting on smtp functionality including an async API

--- mailie/__init__.py
+++ mailie/__init__.py
@@ -1,10 +1,11 @@
 import importlib
 import logging
 
+from ._dispatching import ASyncSMTPClient
+from ._dispatching import SMTPClient
 from ._email import Email
 from ._header import EmailHeader
 from ._policy import POLICIES
-from ._senders import MailSender
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -12,4 +13,4 @@ log.addHandler(logging.NullHandler())
 
 version = importlib.metadata.version("mailie")  # type: ignore [attr-defined]
 
-__all__ = ["Email", "POLICIES", "version", "EmailHeader", "MailSender"]
+__all__ = ["Email", "POLICIES", "version", "EmailHeader", "SMTPClient", "ASyncSMTPClient"]
