98160f9fbf2ebe3aafe920d0e98f329c38e669b5
symonk <jackofspaces@gmail.com>
2022-06-12 17:41:02 +0100

    successfully stubbing smtp.send-message, requires a real client tho

--- tests/core/integration/test_sendmessage.py
+++ tests/core/integration/test_sendmessage.py
@@ -1,13 +1,10 @@
-import pytest
+from pytest_mock import MockerFixture
 
 
-@pytest.mark.skip
-def test_send_message_rcp_to(integration_mail_server, email_factory, sync_client, mocker):
-        mock_smtp = mocker.patch("smtplib.SMTP.send_message", autospec=True)
-        mock_smtp.return_value = {}
-        with sync_client() as client:
-            email = email_factory(mail_from="foo@bar.com", rcpt_to=("a@one.com", "b@two.com"))
-            result = client.send(msg=email)
-            raise Exception(result)
-
-
+def test_send_message_from_addr(integration_mail_server, email_factory, sync_client, mocker: MockerFixture):
+    mock_smtp = mocker.patch("smtplib.SMTP.send_message")
+    with sync_client(port=9222) as client:
+        email = email_factory(mail_from="foo@bar.com", rcpt_to=("a@one.com", "b@two.com"))
+        client.send(email=email, from_addr="fake@stub.com")
+        assert mock_smtp.called
+        assert mock_smtp.call_args[-1]['from_addr'] == "fake@stub.com"
